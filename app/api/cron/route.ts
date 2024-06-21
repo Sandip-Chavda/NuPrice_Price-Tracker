fa955850f80ea5848b30d2e23f53409ad6434400
Sandip-Chavda <chavdas766@gmail.com>
2024-06-21 10:50:10 +0530

    update2

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -104,9 +104,9 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-// export const maxDuration = 55; // This function can run for a maximum of 300 seconds
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
+export const maxDuration = 55; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
